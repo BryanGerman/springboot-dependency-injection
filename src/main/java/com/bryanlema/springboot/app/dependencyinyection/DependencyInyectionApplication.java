d9540ef0bc76bc192af8c788f4031e92742f9b81
bryanlema <bryanglema2@gmail.com>
2024-01-19 10:34:10 -0500

    Creación del proyecto

---
+++ src/main/java/com/bryanlema/springboot/app/dependencyinyection/DependencyInyectionApplication.java
@@ -0,0 +1,13 @@
+package com.bryanlema.springboot.app.dependencyinyection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyInyectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DependencyInyectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bryanlema/springboot/app/dependencyinyection/DependencyInyectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bryanlema.springboot.app.dependencyinyection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyInyectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
